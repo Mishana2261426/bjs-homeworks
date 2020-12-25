cbe56ec9e4c0fd643466126b5158250562690ccc
Misha <misha2261426@gmail.com>
2020-12-25 16:18:21 +0300

    asd

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,35 @@
+"use strict"
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
-}
+	let x =[];
 
+    let d =(b**2-4*a*c);
+    if (d===0) {
+    	x[0] = -b/2*a;
+    }
+    else if (d<0) {
+
+    }
+    else {
+        x[0]=(- b + Math.sqrt(d)) / (2 * a);
+        x[1]=(- b - Math.sqrt(d)) / (2 * a); 
+    }
+
+    return x;
+}
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    if (marks.length == 0) {
+    	return 0;
+    }
+
+    if (marks.length > 5) {
+    	return "оценок больше 5";
+    }
+
+
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
-}
+// function askDrink(name,dateOfBirthday){
+//     // код для задачи №3 писать здесь
+//     // return result;
+// }
