ec93d97922efc70392007c8394c78b804c2f6738
Misha <misha2261426@gmail.com>
2021-01-06 14:09:43 +0300

    asd

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,28 +2,29 @@
 
 function getResult(a,b,c){
 	let x =[];
-
     let d =(b**2-4*a*c);
     if (d===0) {
-    	x[0] = -b/2*a;
+        x[0] = -b/2*a;
     }
-    else if (d<0) {
-
-    }
-    else {
+    else if (d>0){
         x[0]=(- b + Math.sqrt(d)) / (2 * a);
         x[1]=(- b - Math.sqrt(d)) / (2 * a); 
     }
-
     return x;
 }
-function getAverageMark(marks){
-    if (marks.length == 0) {
-    	return 0;
-    }
 
-    if (marks.length > 5) {
-    	return "оценок больше 5";
+function getAverageMark(marks){
+    
+    if (marks.length === 0) {
+        return 0;
+    }
+    else {
+        marks.splice(5);
+    let summ = 0;
+    for (let i = 0; i < marks.length; i++){
+        summ += marks[i];
+    }
+    return (summ/marks.length);
     }
 
 
