1510a96a66d449e48901bfbc5dd019b375cb8dcc
Misha <misha2261426@gmail.com>
2021-04-11 12:39:51 +0300

    asd

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,28 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+    let thisLow = this.toLowerCase().replace(/\s/g, '');
+    return thisLow === thisLow.split('').reverse().join('');
+}
+
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+    let sum = 0;
+    for (let i = 0; i < marks.length; ++i) {
+        sum += marks[i];
+        average = sum / marks.length;
+    }
+    let roundedAverage = Math.ceil(average);
+
+    return averageMark = roundedAverage;
 }
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
-    // return verdict
+    let now = new Date();
+    let happyBirthday = new Date(birthday);
+    let diff = Math.floor(now.getTime() - happyBirthday.getTime());
+    let age = Math.floor(diff / 31536000000);
+    console.log(age);
+
+    return result = age > 18;
 }
